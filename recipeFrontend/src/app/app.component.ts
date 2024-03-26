b1634b23798d84ff6a0752b4206653fb46bd9a70
praakhya <67508200+praakhya@users.noreply.github.com>
2024-03-26 21:14:03 +0530

    Added

--- recipeFrontend/src/app/app.component.ts
+++ recipeFrontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import {MatTabsModule} from '@angular/material/tabs';
+import { RecipePageComponent } from './recipe-page/recipe-page.component';
+import { FavouritePageComponent } from './favourite-page/favourite-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatTabsModule, RecipePageComponent, FavouritePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
