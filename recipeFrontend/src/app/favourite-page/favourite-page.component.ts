78c8183a938b72ff16803f5c98adc59da505e36f
praakhya <67508200+praakhya@users.noreply.github.com>
2024-03-26 21:14:40 +0530

    Added`

---
+++ recipeFrontend/src/app/favourite-page/favourite-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favourite-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './favourite-page.component.html',
+  styleUrl: './favourite-page.component.scss'
+})
+export class FavouritePageComponent {
+
+}
