fe5f7928815bb26968ac5d337ca76eda2d0095b0
praakhya <67508200+praakhya@users.noreply.github.com>
2024-03-26 21:49:48 +0530

    Fixed

--- recipeFrontend/src/app/recipe-page/recipe-page.component.ts
+++ recipeFrontend/src/app/recipe-page/recipe-page.component.ts
@@ -1,12 +1,44 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+import { Recipe } from '../../Models/recipe';
+import { RecipeComponent } from '../recipe/recipe.component';
 @Component({
   selector: 'app-recipe-page',
   standalone: true,
-  imports: [],
+  imports: [RecipeComponent],
   templateUrl: './recipe-page.component.html',
   styleUrl: './recipe-page.component.scss'
 })
 export class RecipePageComponent {
+  recipes:Recipe[] = [];
+  constructor(private httpClient:HttpClient){}
+  ngOnInit() {
+    this.getAllRecipes().subscribe((recipes)=>{
+      this.recipes = recipes
+    })
+  }
+  getHttpOptions():HttpHeaders {
+
+    return new HttpHeaders()
+      .set('Content-Type', 'application/json; charset=utf-8')
+    }
+  
+  createRecipe(recipe: Recipe) {
+
+    return this
+      .httpClient
+      .post<Recipe>("/api/recipe", recipe, {headers:this.getHttpOptions()})
+  }
+  getAllRecipes() {
+
+    return this
+      .httpClient
+      .get<Recipe[]>("/api/recipe", {headers:this.getHttpOptions()})
+  }
+  getRecipeByID(id:Number) {
+    return this
+      .httpClient
+      .get<Recipe>("/api/recipe/" + id, {headers:this.getHttpOptions()})
+  }
 
 }
--- recipeFrontend/src/app/recipe/recipe.component.ts
+++ recipeFrontend/src/app/recipe/recipe.component.ts
@@ -1,12 +1,15 @@
+import { Input } from '@angular/core';
 import { Component } from '@angular/core';
-
+import { Recipe } from '../../Models/recipe';
+import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-recipe',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.scss'
 })
 export class RecipeComponent {
+  @Input() recipe: Recipe|null = null;
 
 }
